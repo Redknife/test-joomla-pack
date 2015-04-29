339428941f38d556d28ec415b44624d04db07725
Sergey Rogachev <realredknife@gmail.com>
2015-04-29 17:59:32 +0500

    all remake

--- components/com_reviews/reviews.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-defined('_JEXEC') or die;
-
-define('DS', DIRECTORY_SEPARATOR);
-
-$controller = JControllerLegacy::getInstance('Reviews');
-$controller->execute(JFactory::getApplication()->input->get('task'));
-$controller->redirect();
--- templates/blank_j3/send.php
+++
@@ -1,2 +0,0 @@
-<?php defined('_JEXEC') or die('Restricted access');
-require_once(JPATH_BASE.'/modules/mod_jsend/helper.php');
